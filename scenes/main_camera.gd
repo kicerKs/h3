265ab5db259ab6e27223a3a4da6fd0ecb435c08d
kicerKs <osk.ch18@gmail.com>
2024-11-23 22:22:22 +0100

    camera movement

---
+++ scenes/main_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+@export var scroll_speed = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("scroll_down"):
+		position.y += scroll_speed * delta
+	if Input.is_action_pressed("scroll_up"):
+		position.y -= scroll_speed * delta
+	if Input.is_action_pressed("scroll_left"):
+		position.x -= scroll_speed * delta
+	if Input.is_action_pressed("scroll_right"):
+		position.x += scroll_speed * delta
+	position.x = clamp(position.x, limit_left+get_viewport().size.x/2, limit_right-get_viewport().size.x/2)
+	position.y = clamp(position.y, limit_top+get_viewport().size.y/2, limit_bottom-get_viewport().size.y/2)
+	print(position)
---
+++ scenes/world.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var rec = $TileMapLayer.get_used_rect()
+	var off = Vector2i(rec.size.x*64, rec.size.y*64)
+	print(off)
+	$MainCamera.limit_top = 0
+	$MainCamera.limit_bottom = off.y
+	$MainCamera.limit_left = 0
+	$MainCamera.limit_right = off.x
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
