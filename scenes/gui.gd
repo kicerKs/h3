6f55bbc0f77dc09bd4b992cc10ebf460da2ba62a
kicerKs <osk.ch18@gmail.com>
2024-11-23 20:39:47 +0100

    turn system

---
+++ scenes/gui.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_button_pressed() -> void:
+	get_parent().get_node("TurnSystem").new_turn()
+
+func _on_turn_system_update_turn(day: Variant, week: Variant, month: Variant) -> void:
+	$Label.text = "Month: " + str(month) + " Week: " + str(week) + " Day: " + str(day)
---
+++ scenes/turn_system.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var current_day
+var current_week
+var current_month
+
+# Used by things that refresh daily e.g. adding resources like wood
+signal new_day
+# Used by things that refresh weekly e.g. new units available to recruit
+signal new_week
+# Used by GUI elements to update
+signal update_turn(day, week, month)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current_day = 1
+	current_week = 1
+	current_month = 1
+	update_turn.emit(current_day, current_week, current_month)
+
+# Use it e.g. when loading a new game from file
+func setup_turn_system(day, week, month):
+	current_day = day
+	current_week = week
+	current_month = month
+	update_turn.emit(current_day, current_week, current_month)
+
+# Called after clicking new turn in GUI
+func new_turn():
+	current_day += 1
+	new_day.emit()
+	if current_day > 7:
+		current_day = 1
+		current_week += 1
+		new_week.emit()
+		if current_week > 4:
+			current_week = 1
+			current_month += 1
+	update_turn.emit(current_day, current_week, current_month)
