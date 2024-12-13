367de6dae0323dc0e27240e94008610ccbf2b286
kicerKs <osk.ch18@gmail.com>
2024-12-14 00:55:58 +0100

    move pathfinding to separate node

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@onready var tilemap = $TileMapLayer
-@onready var player = $Player
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var rec = $TileMapLayer.get_used_rect()
@@ -15,12 +12,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("mouse_click_left"):
-		# print(tilemap.local_to_map(tilemap.get_local_mouse_position()))
-		# player.position = tilemap.local_to_map(tilemap.get_local_mouse_position())*64
-		var astar_grid = AStarGrid2D.new()
-		astar_grid.region = tilemap.get_used_rect()
-		astar_grid.cell_size = Vector2i(64, 64)
-		astar_grid.update()
-		player.start_moving(astar_grid.get_point_path(tilemap.local_to_map(player.position), tilemap.local_to_map(tilemap.get_local_mouse_position())))
-		print(astar_grid.get_point_path(tilemap.local_to_map(player.position), tilemap.local_to_map(tilemap.get_local_mouse_position())))
+	pass
---
+++ scenes/world_pathfinding.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var tilemap: TileMapLayer
+@export var player: Hero
+
+var astar_grid: AStarGrid2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tilemap.get_used_rect()
+	astar_grid.cell_size = Vector2i(64, 64)
+	astar_grid.update()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("mouse_click_left"):
+		player.start_moving(astar_grid.get_point_path(tilemap.local_to_map(player.position), tilemap.local_to_map(tilemap.get_local_mouse_position())))
+		print(astar_grid.get_point_path(tilemap.local_to_map(player.position), tilemap.local_to_map(tilemap.get_local_mouse_position())))
