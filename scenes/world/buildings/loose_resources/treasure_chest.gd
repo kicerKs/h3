781d7cbf72abd297b55017ad44306f07f418843e
kicerKs <osk.ch18@gmail.com>
2025-02-01 19:56:40 +0100

    mam dosc ide na skyrima porobie potem jeszcze

--- scenes/world/buildings/loose_resources/treasure_chest.gd
+++ scenes/world/buildings/loose_resources/treasure_chest.gd
@@ -1,1 +1,46 @@
-extends Sprite2D
+extends Node2D
+
+@onready var tilemap: TileMapLayer = get_node("/root/Main/World/MapMoving")
+var previous_cell
+
+signal pickedup
+var picked = false
+var selection
+var hero: Hero
+
+func _ready():
+	var my_cell = tilemap.local_to_map(position)
+	previous_cell = tilemap.get_cell_source_id(my_cell)
+	tilemap.set_cell(my_cell, 5, Vector2i(0,0))
+	get_node("/root/Main/World/Pathfinding").update()
+
+func pickup():
+	if !picked:
+		print("lol")
+		selection = [[1000, 500], [1500, 1000], [2000, 1500]].pick_random()
+		picked = true
+		pickedup.emit(self, selection)
+
+func chosen(what):
+	if what == 0:
+		Game.Resources.add_resource(GameResources.ResourceTypes.CREDITS, selection[0])
+	else:
+		hero.add_xp(selection[1])
+	remove_from_group("TreasureChests")
+	var my_cell = tilemap.local_to_map(position)
+	tilemap.set_cell(my_cell, previous_cell, Vector2i(0,0))
+	get_node("/root/Main/World/Pathfinding").update()
+	queue_free()
+
+func _on_area_entered(area) -> void:
+	if area.is_in_group("Heroes"):
+		hero = area as Hero
+		hero.inside_something = true
+		hero.connect("interact", pickup)
+
+func _on_area_exited(area) -> void:
+	if area.is_in_group("Heroes"):
+		hero = area as Hero
+		hero.inside_something = false
+		hero.disconnect("interact", pickup)
+		hero = null
---
+++ ui/popups/treasure_chest_popup.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var node: Node2D
+
+var selected = null
+var sel
+
+func _ready():
+	for el in get_tree().get_nodes_in_group("TreasureChests"):
+		el.connect("pickedup", nowy)
+
+func nowy(n: Node2D, selection):
+	print("AHA?")
+	self.visible = true
+	selected = null
+	sel = selection
+	node = n
+	%Title.text = "You picked up a treasure chest!"
+	%Desc.text = "Choose wisely..."
+	%CreditsButton.text = str(selection[0])
+	%ExperienceButton.text = str(selection[1])
+
+func _on_approve_button_pressed() -> void:
+	if selected != null:
+		node.chosen(selected)
+	self.visible = false
+
+func _on_credits_button_pressed() -> void:
+	selected = 0
+
+func _on_experience_button_pressed() -> void:
+	selected = 1
